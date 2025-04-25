b7c113c35f700222472ce0fd28886d7785a7f724
yanglixia <yanglixia568@163.com>
2025-04-25 21:57:52 +0800

    add: 最长公共子序列

--- 算法及编程实现/动态规划/LCR_095_最长公共子序列.js
+++ 算法及编程实现/动态规划/LCR_095_最长公共子序列.js
@@ -26,12 +26,38 @@
   text1 和 text2 仅由小写英文字符组成。
  */
 
-
+/**
+ * 思路：
+ * f(i, j)表示第1个字符串中下标 从0到i 的字符串（记为：s1[0, ..., i]） 和 第2个字符串中下标 从0到j 的字符串（记为：s2[0, ..., j]）的最长公共子序列的长度！
+ * 若第1个字符串的最大长度是m，第2个字符串最大长度为n，则f(m-1, n-1)就是我们要求的最长公共子序列的长度！
+ *
+ * 若s1[i] === s2[j]，则f(i, j) = f(i-1, j-1) + 1
+ * 否则，f(i, j) = max(f(i-1, j), f(i, j-1))
+ * 
+ * TODO:若i,j为0时，会取到f(-1,-1)、f(0, -1)或f(-1,0)的值，所以dp长度为len1+1
+ */
 /**
 * @param {string} text1
 * @param {string} text2
 * @return {number}
 */
 var longestCommonSubsequence = function (text1, text2) {
-
+  let len1 = text1.length;
+  let len2 = text2.length;
+  let dp = new Array(len1 + 1).fill(0).map(() => new Array(len2 + 1).fill(0));  // TODO:为什么是len1 + 1? 答案如上
+  for (let i = 0; i < len1; i++) {
+    for (let j = 0; j < len2; j++) {
+      if (text1[i] === text2[j]) {
+        dp[i + 1][j + 1] = dp[i][j] + 1;
+      } else {
+        dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+      }
+    }
+  }
+  // console.log(dp);
+  return dp[len1][len2];
 };
+
+console.log(longestCommonSubsequence("abcde", "ace")); // 3
+console.log(longestCommonSubsequence("abc", "abc")); // 3
+console.log(longestCommonSubsequence("abc", "def")); // 0
--- 算法及编程实现/常用技巧.js
+++ 算法及编程实现/常用技巧.js
@@ -12,7 +12,4 @@ const vis2 = new Array(arr.length).fill(false);
  */
 const arr = Array.from({ length: nums.length }, (_, i) => i + 1);
 
-/**
- * 子串与子序列： 子串要求字符连续出现，而子序列则不要求字符连续。
- * 如: 字符串abcd, 子串有ab, bc, cd, abc, abcd等，子序列有a, b, c, d, ac, ad, bc, bd, cd等。
- */
+
